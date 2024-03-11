9cc9da05eb88d512b9808d2ff47d35df5a645e67
Probošt Vojtěch <vojtaproba@seznam.cz>
2024-03-11 19:16:28 +0100

    todo fix deleteRecord()

--- record.js
+++ record.js
@@ -1,4 +1,4 @@
-const addRecordDialog = document.querySelector("dialog");
+const addRecordDialog = document.querySelector("dialog.addRecord");
 const form = document.querySelector('form');
 const startHoursInput = document.querySelector("#startHours");
 const startMinutesInput = document.querySelector("#startMinutes");
@@ -6,8 +6,10 @@ const endHoursInput = document.querySelector("#endHours");
 const endMinutesInput = document.querySelector("#endMinutes");
 const taskInput = document.querySelector('#task');
 const projectSelect = document.querySelector('#project');
-let newRecordBtn = document.querySelector('.newRecordBtn');
+const newRecordBtn = document.querySelector('.newRecordBtn');
 const recordsTbody = document.querySelector('.recordsTbody');
+const timeTodaySpan = document.querySelector('.timeToday');
+let timeToday;
 let records = [];
 
 
@@ -15,21 +17,33 @@ function addRecord() {
     let selectedProject = projectSelect.options[projectSelect.selectedIndex].innerText;
     let startTime = new Date().setHours(startHoursInput.value, startMinutesInput.value, 0, 0);
     let endTime = new Date().setHours(endHoursInput.value, endMinutesInput.value, 0, 0);
-    let totalTime = endTime -startTime;
-
-    let startTimeString = new Date(startTime).toLocaleTimeString('cs-cz')
-    let endTimeString = new Date(endTime).toLocaleTimeString('cs-cz')
-    let totalTimeString = new Date(totalTime).toLocaleTimeString('cs-cz')
+    let totalTime = endTime - startTime
 
     const record = {
+        index: 0,
         task: taskInput.value,
         project: selectedProject,
-        start: startTimeString,
-        end: endTimeString,
-        total: totalTimeString
+        start: startTime,
+        end: endTime,
+        total: totalTime
     }
+    //set id to index in array
+    record.index = records.length;
     records.push(record);
     saveRecordsToLocal();
+
+    timeToday += totalTime;
+    saveTimeToday()
+}
+
+function deleteRecord(index) {
+    if (index >= 0 && index < records.length) {
+        timeToday -= records[index].total;
+        records.splice(index, 1);
+        saveRecordsToLocal();
+        saveTimeToday()
+        generateHtmlRecords()
+    }
 }
 
 function saveRecordsToLocal() {
@@ -39,15 +53,26 @@ function saveRecordsToLocal() {
     localStorage.setItem('records', recordsJson);
 }
 
+function saveTimeToday() {
+    localStorage.setItem('timeToday', timeToday.toString());
+}
+
 function loadRecordsFromLocal() {
     //load from local storage
     const recordsJson = localStorage.getItem('records');
     if (recordsJson == null) {
-        console.log('local storage null')
         return;
     }
     //parse back into array of records
     records = JSON.parse(recordsJson);
+}
+
+function loadTimeToday() {
+    let timeTodayString = localStorage.getItem('timeToday');
+    if (timeTodayString == null) {
+        return;
+    }
+    timeToday = parseInt(timeTodayString);
 }
 
 function generateHtmlRecords() {
@@ -56,30 +81,44 @@ function generateHtmlRecords() {
     allTr.forEach((tr) => tr.remove());
 
     records.forEach((r) => {
-        const recordTr = document.createElement('tr')
-        const taskTd = document.createElement('td')
-        const projectTd = document.createElement('td')
-        const startTd = document.createElement('td')
-        const endTd = document.createElement('td')
-        const totalTd = document.createElement('td')
+        const recordTr = document.createElement('tr');
+        const taskTd = document.createElement('td');
+        const projectTd = document.createElement('td');
+        const startTd = document.createElement('td');
+        const endTd = document.createElement('td');
+        const totalTd = document.createElement('td');
+        const actionTd = document.createElement('td');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'smazat';
+        deleteBtn.addEventListener('click', () => {
+            deleteRecord(r.index);
+            timeToday -= r.total;
+            console.log(r.index)
+            saveTimeToday();
+            generateHtmlRecords();
+        })
 
         taskTd.innerText = r.task;
         projectTd.innerText = r.project;
-        startTd.innerText = r.start;
-        endTd.innerText = r.end;
-        totalTd.innerText = r.total;
+        startTd.innerText = new Date(r.start).toLocaleTimeString('cs-cz');
+        endTd.innerText = new Date(r.end).toLocaleTimeString('cs-cz')
+        totalTd.innerText = new Date(r.total).toUTCString().split(' ')[4];
         recordTr.appendChild(taskTd);
         recordTr.appendChild(projectTd);
         recordTr.appendChild(startTd);
         recordTr.appendChild(endTd);
         recordTr.appendChild(totalTd);
+        recordTr.appendChild(actionTd);
+        actionTd.appendChild(deleteBtn);
 
         recordsTbody.appendChild(recordTr);
+        timeTodaySpan.innerText = new Date(timeToday).toUTCString().split(' ')[4];
     })
 }
 
 //load and generate html initially
 loadRecordsFromLocal()
+loadTimeToday()
 generateHtmlRecords()
 
 //button shows dialog
@@ -87,6 +126,7 @@ newRecordBtn.addEventListener('click', () => {
     addRecordDialog.show();
 })
 
+//new record submitted in dialog
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     addRecord();
